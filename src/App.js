5c79acf7439f0557ae75d503ec7eb50eaea9d8f1
Rahim Ansari <rahimansari4560@gmail.com>
2022-11-07 06:49:25 +0530

    Created Folder Structure For Todo App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { useState } from "react";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+export default App;
