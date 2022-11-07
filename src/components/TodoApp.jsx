fa63a0d69d41c89b3d554f8242355ad0e33f2609
Rahim Ansari <rahimansari4560@gmail.com>
2022-11-07 06:50:45 +0530

    Done With Main TodoApp Folder

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+import TodoItem from "./TodoItem";
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+  const deleteTodo = (value) => {
+    setTodos(todos.filter((todo) => todo !== value));
+  };
+
+  return (
+    <div className="border">
+      <TodoInput addTodo={addTodo} />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem value={todo} deleteTodo={deleteTodo} />
+        ))}
+      </TodoList>
+    </div>
+  );
+};
+export default TodoApp;
